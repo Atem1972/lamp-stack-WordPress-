56018396c58cb3fd47c2856c763431efb9219bd0
Atem1972 <154833460+Atem1972@users.noreply.github.com>
2024-06-24 12:28:00 +0200

    Update maiin.tf

--- maiin.tf
+++ maiin.tf
@@ -1,65 +1,135 @@
-#!/bin/bash
+# configured aws provider with proper credentials
+provider "aws" {
+  region    = var.aws_region
+  profile   = "default"
+}
+# Create a VPC
+resource "aws_vpc" "lamp-vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Lamp VPC"
+  }
+}
 
-# Set mysql config variables
-db_username="oracle"
-db_user_password="school1"
-db_name="webserver"
+# Create Web Public Subnet
+resource "aws_subnet" "lamp-subnet" {
+  vpc_id                  = aws_vpc.lamp-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Name = "lamp-subnet"
+  }
+}
 
-# update the centos 7 server
-sudo yum update -y
-# install apache, mysql, php and its additionnal packages
-sudo yum install httpd mariadb-server php php-mysql php-gd -y
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.lamp-vpc.id
 
-# start the httpd daemon
-sudo systemctl start httpd
+  tags = {
+    Name = "Lamp IGW"
+  }
+}
 
-# enable the httpd daemon
-sudo systemctl enable httpd
+# Create Web layer route table
+resource "aws_route_table" "web-rt" {
+  vpc_id = aws_vpc.lamp-vpc.id
 
-# get the status of htpd daemon
-sudo systemctl status httpd
 
-# start mysql service 
-sudo systemctl start mariadb
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
-# enable mysql service
-sudo systemctl enable mariadb
+  tags = {
+    Name = "lamp WebRT"
+  }
+}
 
-# Change OWNER and permission of directory /var/www
-usermod -a -G apache ec2-user
-sudo chown -R ec2-user:apache /var/www
-find /var/www -type d -exec chmod 2775 {} \;
-find /var/www -type f -exec chmod 0664 {} \;
+# Create Web Subnet association with Web route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.lamp-subnet.id
+  route_table_id = aws_route_table.web-rt.id
+ }
 
-# Download wordpress package and extract
-sudo yum install wget -y
-wget http://wordpress.org/wordpress-5.1.1.tar.gz
-tar -xzvf wordpress-5.1.1.tar.gz
-rm -f wordpress-5.1.1.tar.gz
-cp -r wordpress/* /var/www/html/
+# Create Web Security Group
+resource "aws_security_group" "lamp-sg" {
+    name        = "Lamp security group"
+    description = "Allow ssh inbound traffic"
+    vpc_id      = aws_vpc.lamp-vpc.id
+  
+    ingress {
+      description = "ssh from VPC"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    ingress {
+    description = "http port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+    tags = {
+      Name = "Lamp-SG"
+    }
+}
 
-# AUTOMATIC mysql_secure_installation
+# use data source to get a registered amazon linux 2 ami
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
 
-# Create database, database user and grant privileges
-mysql -u root -e "CREATE DATABASE $db_name;"
-mysql -u root -e "CREATE USER '$db_username'@'localhost' IDENTIFIED BY '$db_user_password';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_username'@'localhost';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
 
-# Create wordpress configuration file and update database value
-cd /var/www/html
-cp wp-config-sample.php wp-config.php
+#create ec2 instance for Lamp stack
 
-sed -i "s/database_name_here/$db_name/g" wp-config.php
-sed -i "s/username_here/$db_username/g" wp-config.php
-sed -i "s/password_here/$db_user_password/g" wp-config.php
-cat <<EOF >>/var/www/html/wp-config.php
+resource "aws_instance" "lamp_server" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = var.aws_instance_type
+  subnet_id              = aws_subnet.lamp-subnet.id
+  vpc_security_group_ids = [aws_security_group.lamp-sg.id]
+  key_name               = aws_key_pair.lamp_key.key_name
+  user_data              = file("install_lamp.sh")
+   
+  tags = {
+    owner   = "Lamp-estephe"
+    Environment = "dev"
+    Name  = "lamp stack"
+  }
+}
+# an empty resource block
+resource "null_resource" "name" {
 
-define( 'FS_METHOD', 'direct' );
-define('WP_MEMORY_LIMIT', '256M');
-EOF
-
-# Enable apache and mysql and restart apache
-sudo systemctl enable  httpd
-sudo systemctl enable mariadb
-sudo systemctl restart httpd
+  # ssh into the ec2 instance 
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file(local_file.ssh_key.filename)
+    host        = aws_instance.lamp_server.public_ip
+  }
+  # wait for ec2 to be created
+  depends_on = [aws_instance.lamp_server]
+}
